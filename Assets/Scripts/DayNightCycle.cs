5406c6b13b78e679faa9434b8f24715d26190342
Alfie Edwards <alfie-edwards@hotmail.com>
2026-07-26 18:31:09 +0100

    Fix scene reset

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -20,6 +20,8 @@ public class DayNightCycle : MonoBehaviour
     public float sunriseTransitionDuration = 3f; // Configurable duration for the start day transition
     public float sunsetTransitionDuration = 15f;  // Configurable duration for the gradual sunset transition
     public float trueNightTransitionDuration = 3f; // Configurable duration for the quick drop to true night
+    [SerializeField] private GameObject wildernessPrefab;
+    [SerializeField] private GameObject currentWilderness;
 
     [Header("Smooth Color Cycle")]
     public Color dayColor = Color.white;
@@ -49,6 +51,20 @@ public class DayNightCycle : MonoBehaviour
                 SetNight();
                 StartTrueNightTransition();
             }
+        }
+    }
+
+    public void ResetWilderness()
+    {
+        if (currentWilderness != null && wildernessPrefab != null)
+        {
+            Transform t = currentWilderness.transform;
+            Vector3 pos = t.position;
+            Quaternion rot = t.rotation;
+            Transform parent = t.parent;
+
+            Destroy(currentWilderness);
+            currentWilderness = Instantiate(wildernessPrefab, pos, rot, parent);
         }
     }
 
@@ -198,6 +214,7 @@ public class DayNightCycle : MonoBehaviour
         {
             Destroy(enemy.gameObject);
         }
+        ResetWilderness();
         isNight = false;
         timeRemainingS = dayLengthS;
         sunriseTimer = 0f;
--- Assets/Scripts/PlantSpot.cs
+++ Assets/Scripts/PlantSpot.cs
@@ -19,7 +19,7 @@ public class PlantSpot : MonoBehaviour
             Grow();
         }
         if (Growing) {
-            for (int i = 0; i != 50; ++i)
+            for (int i = 0; i != 30; ++i)
             {
                 Instantiate(goldPrefab);
             }
